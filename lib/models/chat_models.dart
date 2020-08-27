0e781739b0dc16fbf98a223291197220ac4eb5fc
Aaris Kazi <40735215+Aaris-Kazi@users.noreply.github.com>
2020-08-27 14:43:06 +0530

    Add files via upload

--- lib/models/chat_models.dart
+++ lib/models/chat_models.dart
@@ -5,12 +5,17 @@ class ChatModel {
   final String avatar;
 
   ChatModel({this.name, this.message, this.time, this.avatar});
-  List<ChatModel> dummydata = [
-    new ChatModel(
-      name: "Dolores Abernathy",
-      message: "Welcome your new GOD",
-      time: "00:00",
-      avatar: "images/Dolores.jpg"
-    )
-  ];
 }
+
+List<ChatModel> dummydata = [
+  new ChatModel(
+      name: "Dolores Abernathy",
+      message: "Welcome to West World",
+      time: "11:00",
+      avatar: "images/Dolores.jpg"),
+  new ChatModel(
+      name: "Dolores Abernathy",
+      message: "I'm just a daughter of a rancher",
+      time: "00:00",
+      avatar: "images/Dolores_Passed_Pawn.jpg")
+];
